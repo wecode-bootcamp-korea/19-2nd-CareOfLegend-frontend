86bfa14eca26f767418cb668622dc7eb7a829341
민석 / MIN / ミンソク <55354735+minsoku@users.noreply.github.com>
2021-04-26 16:52:41 +0900

    초기세팅완료

---
+++ src/Config.js
@@ -0,0 +1,8 @@
+const API = '';
+export const LOGIN_API = ``;
+export const SIGNUP_API = ``;
+export const MAIN_API = ``;
+export const QUIZ_API = ``;
+export const PRODUCTS_API = ``;
+export const PRODUCT_API = ``;
+export const CART_API = ``;
---
+++ src/Routes.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import Main from './Pages/Main/Main';
+import Cart from './Pages/Cart/Cart';
+import Login from './Pages/Login/Login';
+import ProductDetail from './Pages/ProductDetail/ProductDetail';
+import ProductList from './Pages/ProductList/ProductList';
+import Quiz from './Pages/Quiz/Quiz';
+import Signup from './Pages/Signup/Signup';
+
+class Routes extends Component{
+  render(){
+    return(
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Main}/>
+          <Route exact path="/Cart" component={Cart}/>
+          <Route exact path="/login" component={Login}/>
+          <Route exact path="/productdetail" component={ProductDetail}/>
+          <Route exact path="/productlist" component={ProductList}/>
+          <Route exact path="/quiz" component={Quiz}/>
+          <Route exact path="/signup" component={Signup}/>
+        </Switch>
+      </Router>
+    )
+  }
+}
